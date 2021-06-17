5cd69309a3911007cf2963b753b002617e92589b
Donovon Spencer <donovon.spencer@gmail.com>
2021-06-17 09:21:13 -0600

    Initial Commit

---
+++ Shared/MessageMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageMakerApp.swift
+//  Shared
+//
+//  Created by Donovon Spencer on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct MessageMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
