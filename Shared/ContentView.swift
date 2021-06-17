de341d88429fe85878e879d24580e4fa602d74b7
Donovon Spencer <donovon.spencer@gmail.com>
2021-06-17 12:55:43 -0600

    Working on share sheet right now

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,18 +7,53 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var transformText: String = "Welcome!"
-
+struct ImageView: View {
     var body: some View {
         VStack {
-            Text(transformText)
-                .padding()
-            
-            TextField("Placeholder", text: $transformText)
-                .border(Color(UIColor.separator))
+            Image("WhiteBackground")
+                .resizable()
+                .scaledToFit()
+            Button(action: shareImage, label: {
+                Text("Share")
+                    .padding()
+                    .background(Color(red: 0, green: 0, blue: 0.5))
+                    .foregroundColor(.white)
+                    .clipShape(Capsule())
+            })
         }
-        .textFieldStyle(RoundedBorderTextFieldStyle())
+    }
+    
+    func shareImage() {
+        let image = (UIImage(named: "WhiteBackground"))
+        let av = UIActivityViewController(activityItems: [image!], applicationActivities: nil)
+        av.isModalInPresentation = true
+        
+        UIApplication.shared.windows.first?.rootViewController?.present(av, animated: true, completion: nil)
+    }
+}
+
+struct ContentView: View {
+    @State private var transformText: String = ""
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text(transformText)
+                    .padding()
+                TextField("Text to strech", text: $transformText)
+                    .border(Color(UIColor.separator))
+                NavigationLink(
+                    destination: ImageView(),
+                    label: {
+                        Text("Create Image")
+                            .padding()
+                            .background(Color(red: 0, green: 0, blue: 0.5))
+                            .foregroundColor(.white)
+                            .clipShape(Capsule())
+                    })
+            }
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+        }
     }
 }
 
